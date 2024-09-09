5ff4832fcce94765c90c96c4ab9abb365cf600a8
Bownie <marketing@letcode.com.br>
2024-09-09 09:03:02 -0300

    Desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +23,22 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public void ExibirNumero(){
+            Console.WriteLine("Numero: " + Numero);
+        }
+
+        public void ExibirModelo(){
+            Console.WriteLine("Modelo: " + Modelo);
+        }
+
+        public void ExibirIMEI(){
+            Console.WriteLine("IMEI: " + IMEI);
+        }
+
+        public void ExibirMemoria(){
+            Console.WriteLine("Memoria: " + Memoria);
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("73 998267866","Iphone","Imei123", 32);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+iphone.ExibirIMEI();
+iphone.ExibirMemoria();
+iphone.ExibirModelo();
+iphone.ExibirNumero();
+
+Console.WriteLine("\n");
+
+Nokia nokia = new Nokia("71999603264", "Nokia", "Imei456", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.ExibirIMEI();
+nokia.ExibirMemoria();
+nokia.ExibirModelo();
+nokia.ExibirNumero();
